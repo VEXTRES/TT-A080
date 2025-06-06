ee756ef9fa59f54bd44d203c99e0b1a24d3df623
Uriel Sanchez <uriel.ss@hotmail.com>
2025-06-05 19:27:43 -0600

    retorna automaticamente a nutricion si detecta /

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,9 @@ use Illuminate\Support\Facades\Route;
 
 
 
-    Route::get('/', function () {
-        return view('welcome');
-    });
+Route::get('/', function () {
+    return redirect()->route('nutricion');
+});
 
 Route::controller(InicioController::class)->group(function () {
     Route::get('/nutricion', 'nutricion')->name('nutricion');
